bd6c3745ddf960c676eb35bad9e8148d5cd949b5
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-27 10:26:47 +0530

    Adding a new Activity for Notes

---
+++ app/src/main/java/com/satyamthakur/learning/notewhiz/AddNodeActivity.kt
@@ -0,0 +1,11 @@
+package com.satyamthakur.learning.notewhiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_node)
+    }
+}
