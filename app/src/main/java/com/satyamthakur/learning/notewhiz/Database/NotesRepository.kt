19f591ccde9782727c11d835d28bea71beb660c9
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-29 10:58:46 +0530

    Creating a Repository class with all the suspend methods

---
+++ app/src/main/java/com/satyamthakur/learning/notewhiz/Database/NotesRepository.kt
@@ -0,0 +1,22 @@
+package com.satyamthakur.learning.notewhiz.Database
+
+import androidx.lifecycle.LiveData
+import com.satyamthakur.learning.notewhiz.Models.Note
+
+class NotesRepository(private val noteDao: NoteDao) {
+
+    val allNotes: LiveData<List<Note>> = noteDao.getAllNotes()
+
+    suspend fun insert(note: Note) {
+        noteDao.insert(note)
+    }
+
+    suspend fun delete(note: Note) {
+        noteDao.delete(note)
+    }
+
+    suspend fun update(id: Int?, title: String?, note: String?) {
+        noteDao.update(id, title, note)
+    }
+
+}
